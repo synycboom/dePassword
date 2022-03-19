7b03f1a54b0637aa2c5ad6692242082fc08d8cb3
synycboom <synycboom@gmail.com>
2022-03-19 18:59:59 +0700

    feat: implement apis

--- fe/src/helpers/api.ts
+++ fe/src/helpers/api.ts
@@ -1,8 +1,21 @@
-import axios from "axios";
-import setting from "../setting";
+import axios from 'axios';
+import setting from '../setting';
 
-export const uploadFileToSwarm = async (encryptedFile: string) => {
-  const url = `${setting.API_URL}/files/upload`;
-  const result = await axios.post(url, { encryptedFile });
-  return result;
-};
+export const uploadFile = async (file: File): Promise<string> => {
+  const formData = new FormData();
+  formData.append('encryptedFile', file);
+
+  const { data: { reference }} = await axios.post(`${setting.SERVER_URL}/v1/files/upload`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+
+  return reference;
+}
+
+export const downloadFile = async (reference: string): Promise<Blob> => {
+  const res = await axios.get(`${setting.SWARM_GATEWAY}/bzz/${reference}`);
+
+  return new Blob([res.data]);
+}
