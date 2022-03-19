d19e7e5b9dcaa514b7fa9c76e1752d64b96a97d5
synycboom <synycboom@gmail.com>
2022-03-19 15:53:02 +0700

    feat: integrate services

---
+++ server/index.ts
@@ -0,0 +1,36 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+import express, { Application } from "express";
+import cors from 'cors';
+import logger from './utils/logger';
+import errorHandler from './errors/handler';
+import fileHandler from './apis/file';
+
+const app: Application = express();
+const port = process.env.PORT || 8080;
+const domains: string[] = [];
+
+app.use(
+  cors({
+    origin: [/localhost:*/, ...domains],
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/v1/files', fileHandler);
+app.use(errorHandler);
+
+async function start() {
+  app.listen(port, (): void => {
+    logger.info(`Running server on port ${port}`);
+  });
+}
+
+start().catch((err) => {
+  logger.error(err.message, { stack: err.stack });
+  process.exit(1);
+});
